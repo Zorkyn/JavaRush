96fb8b6be9708b171e40a374885914ade8b7c727
Zork <zorkyn@gmail.com>
2013-03-06 09:24:52 +0200

    level 9 secret program (task 1)

---
+++ src/com/javarush/test/level09/lesson11/home01/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level09.lesson11.home01;
+
+/**
+ * User: General
+ * Date: 12/21/12
+ * Time: 11:59 PM
+ */
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        try {
+            int x =  5/0;
+            System.out.println(x);
+        } catch (Exception e){
+
+        }
+
+    }
+}
---
+++ src/com/javarush/test/level09/lesson11/home02/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level09.lesson11.home02;
+
+/**
+ * User: General
+ * Date: 12/21/12
+ * Time: 11:59 PM
+ */
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        for (int i = 10; i >= 0; i--)
+        {
+            System.out.println(i);
+
+            //Написать тут ваш код
+        }
+    }
+}
